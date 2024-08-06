bc2d3670fc1d7e751c51e3d47c7c4f612b53a1be
L00ptyl00p <guadalupelucio27@gmail.com>
2024-08-05 23:51:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "47b90c9bbea7cd28cta502ea562f03od";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
