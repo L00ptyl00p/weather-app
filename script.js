fabd641b9be0ddab94455d78da40df9140748500
L00ptyl00p <guadalupelucio27@gmail.com>
2024-08-09 04:07:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -61,9 +61,33 @@ axios(apiUrl).then(displayForecast);
 }
 function displayForecast(response){
   console.log(response.data);
-  let days=["Tues","Wed","Thurs","Fri","Sat"];
-  let forecastHtml="";
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
+
+
+
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
